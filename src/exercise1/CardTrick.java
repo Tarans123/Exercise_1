aa60eb1f0353f1c19f6d32f6d9309e7e7c9482fb
Tarans123 <157844920+Tarans123@users.noreply.github.com>
2024-01-26 17:53:45 -0500

    Update CardTrick.java

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -69,7 +69,7 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        /*System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        /*System.out.println("My name is Taranpreet, but you can call me prof, Taranpreet or sir");
         System.out.println();
         
         System.out.println("My career ambitions:");
